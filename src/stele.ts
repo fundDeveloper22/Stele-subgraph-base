3f19c4bb9eabaf92e655f0ce1ebceb4b3a78ec01
fundDeveloper22 <openfund365@gmail.com>
2025-04-29 16:19:34 +0900

    update subgraph entities

--- src/stele.ts
+++ src/stele.ts
@@ -18,8 +18,6 @@ import {
   AddToken,
   Claim,
   Create,
-  DebugJoin,
-  DebugTokenPrice,
   EntryFee,
   Join,
   MaxAssets,
@@ -36,139 +34,6 @@ export function handleAddToken(event: AddTokenEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.tokenAddress = event.params.tokenAddress
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleClaim(event: ClaimEvent): void {
-  let entity = new Claim(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.challengeId = event.params.challengeId
-  entity.user = event.params.user
-  entity.rewardAmount = event.params.rewardAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCreate(event: CreateEvent): void {
-  let entity = new Create(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.challengeId = event.params.challengeId
-  entity.challengeType = event.params.challengeType
-  entity.startTime = event.params.startTime
-  entity.endTime = event.params.endTime
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDebugJoin(event: DebugJoinEvent): void {
-  let entity = new DebugJoin(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenAddress = event.params.tokenAddress
-  entity.amount = event.params.amount
-  entity.totalRewards = event.params.totalRewards
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleDebugTokenPrice(event: DebugTokenPriceEvent): void {
-  let entity = new DebugTokenPrice(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.baseToken = event.params.baseToken
-  entity.baseAmount = event.params.baseAmount
-  entity.quoteToken = event.params.quoteToken
-  entity.quoteAmount = event.params.quoteAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleEntryFee(event: EntryFeeEvent): void {
-  let entity = new EntryFee(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.newEntryFee = event.params.newEntryFee
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleJoin(event: JoinEvent): void {
-  let entity = new Join(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.challengeId = event.params.challengeId
-  entity.user = event.params.user
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleMaxAssets(event: MaxAssetsEvent): void {
-  let entity = new MaxAssets(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.newMaxAssets = event.params.newMaxAssets
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRegister(event: RegisterEvent): void {
-  let entity = new Register(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.challengeId = event.params.challengeId
-  entity.user = event.params.user
-  entity.performance = event.params.performance
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -216,6 +81,78 @@ export function handleSeedMoney(event: SeedMoneyEvent): void {
   entity.save()
 }
 
+export function handleEntryFee(event: EntryFeeEvent): void {
+  let entity = new EntryFee(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.newEntryFee = event.params.newEntryFee
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMaxAssets(event: MaxAssetsEvent): void {
+  let entity = new MaxAssets(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.newMaxAssets = event.params.newMaxAssets
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleCreate(event: CreateEvent): void {
+  let entity = new Create(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.challengeId = event.params.challengeId
+  entity.challengeType = event.params.challengeType
+  entity.startTime = event.params.startTime
+  entity.endTime = event.params.endTime
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleJoin(event: JoinEvent): void {
+  let entity = new Join(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.challengeId = event.params.challengeId
+  entity.user = event.params.user
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
 export function handleSwap(event: SwapEvent): void {
   let entity = new Swap(
     event.transaction.hash.concatI32(event.logIndex.toI32())
@@ -228,6 +165,36 @@ export function handleSwap(event: SwapEvent): void {
   entity.fromPriceUSD = event.params.fromPriceUSD
   entity.toPriceUSD = event.params.toPriceUSD
   entity.toAmount = event.params.toAmount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRegister(event: RegisterEvent): void {
+  let entity = new Register(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.challengeId = event.params.challengeId
+  entity.user = event.params.user
+  entity.performance = event.params.performance
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleClaim(event: ClaimEvent): void {
+  let entity = new Claim(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.challengeId = event.params.challengeId
+  entity.user = event.params.user
+  entity.rewardAmount = event.params.rewardAmount
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
